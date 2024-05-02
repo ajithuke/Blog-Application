572b6e0e80d3bda7dc242b6c35717579c726fcdb
Ajit Huke <ajithuke63@gmail.com>
2024-05-02 18:00:06 +0530

    completed project

--- routes/blog.js
+++ routes/blog.js
@@ -36,9 +36,11 @@ router.post('/',upload.single('coverImage'),async (req,res)=>{
 
 router.get('/:id',async (req,res)=>{
     const blog = await Blog.findById(req.params.id).populate('createdBy')
+    const comments = await Comment.find({blogId:req.params.id}).populate('createdBy')
     res.render('blog',{
         user:req.user,
         blog,
+        comments
     })
 })
 
